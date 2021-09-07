5566b1e28e782bc64f334e824fbb55d2d65b688b
Caleb Boylan <calebboylan@gmail.com>
2021-09-06 19:04:40 -0700

    Add lyon plugin package

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::render::mesh::Indices;
+use bevy::render::pipeline::PrimitiveTopology;
 
 mod debug;
 
@@ -7,24 +9,31 @@ struct Player;
 const SCREEN_WIDTH: f32 = 1280.0;
 const SCREEN_HEIGHT: f32 = 720.0;
 
-fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        sprite: Sprite::new(Vec2::new(32.0, 32.0)),
-        ..Default::default()
-    });
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            sprite: Sprite::new(Vec2::new(32.0, 32.0)),
+            ..Default::default()
+        })
+        .insert(Player);
 }
 
 fn main() {
     App::build()
         .insert_resource(WindowDescriptor {
-            //vsync: false,
+            vsync: false,
             width: SCREEN_WIDTH,
             height: SCREEN_HEIGHT,
             ..Default::default()
         })
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(debug::DebugPlugin)
         .add_startup_system(setup.system())
